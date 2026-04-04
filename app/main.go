dd39557b45fe17e2afcf287292e1f6dcb7796c09
Babafemi Jimoh-Kuku <babafemijk@gmail.com>
2026-04-05 00:34:45 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
+
+// Client sends commands to the Redis server as RESP arrays
+// "*3\r\n$3\r\nSET\r\n$4\r\nuser\r\n$5\r\nalice"
+// ["*3", "$3SET", "$4user", "$5alice"]
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	buf := make([]byte, 1024)
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		line := scanner.Text()
 
-	for {
-		_, err := conn.Read(buf)
-		if err != nil {
-			break
+		withoutcrlf := strings.Split(line, "\r\n")
+
+		arguments := []string{}
+		for i, str := range withoutcrlf[1:] {
+			arguments[i] = strings.ReplaceAll(str, "$", "")
+		}
+
+		if strings.ToUpper(arguments[0]) == "ECHO" {
+			strLen := len(arguments[1])
+			resp := fmt.Sprintf("$%d\r\n%s\r\n", strLen, arguments[1])
+			conn.Write([]byte(resp))
 		}
 
 		conn.Write([]byte("+PONG\r\n"))
